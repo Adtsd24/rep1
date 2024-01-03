b27eaf3c5a8229231112ac49cacc1d8980067273
Adtsd24 <155478972+Adtsd24@users.noreply.github.com>
2024-01-03 12:37:20 +0200

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
+int worldWar1 = 1914;
 /* This is multi-line comment, that defines the COUT code!
   Hello World! */
 int main() {
   cout << "Hello World! \n\n";
   cout << "This is a test message! \n\n";
   cout << "Hi! I am a 10 year kid, that is interested in computers!";
+  cout << "The World War 1 started at: ";
+  cout << worldWar1;
   return 0;
 }
