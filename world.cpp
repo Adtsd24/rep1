f33acb14c749f4644bd21e290767d376fc3f6d6f
Adtsd24 <155478972+Adtsd24@users.noreply.github.com>
2024-01-03 12:23:08 +0200

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -4,3 +4,11 @@ int main() {
   cout << "Hello World!";
   return 0;
 }
+int main() {
+  cout << "test";
+  return 1;
+}
+int main() {
+  cout << "Hi! I am 10 year old kid, that likes computers!";
+  return 2;
+}
