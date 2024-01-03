ca3bc15640df328433c20f0daef344962b9a673f
Adtsd24 <155478972+Adtsd24@users.noreply.github.com>
2024-01-03 12:28:24 +0200

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-  cout << "Hello World! \n";
-  cout << "test \n";
+  cout << "Hello World! \n\n";
+  cout << "This is a test message! \n\n";
   cout << "Hi! I am a 10 year kid, that is interested in computers!";
   return 0;
 }
