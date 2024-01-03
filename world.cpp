8fadbd8a54d19d2fe4a0d545aa20ea805a09a2fe
Adtsd24 <155478972+Adtsd24@users.noreply.github.com>
2024-01-03 12:30:53 +0200

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+/* This is multi-line comment, that defines the COUT code!
+  Hello World! */
 int main() {
   cout << "Hello World! \n\n";
   cout << "This is a test message! \n\n";
